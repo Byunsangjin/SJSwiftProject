e9727d23c1da05b5ad7c453db1039fdc2116b452
byunsangjin <qustkdwls1@naver.com>
2019-04-16 11:43:24 +0900

    Project Init

---
+++ SJSwiftProject/SJSwiftProject/ViewControllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SJSwiftProject
+//
+//  Created by Byunsangjin on 16/04/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
+class tableCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    
+}
+
