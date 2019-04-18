976898aa28b3cc137380ff5840f8090ad0c3f029
byunsangjin <qustkdwls1@naver.com>
2019-04-19 01:15:19 +0900

    Gradation boder설정 추가

--- SJSwiftProject/SJSwiftProject/Gradation/View/GradationView.swift
+++ SJSwiftProject/SJSwiftProject/Gradation/View/GradationView.swift
@@ -10,19 +10,37 @@ class GradationView: UIView {
         }
     }
     
-    @IBInspectable var endColor: UIColor = UIColor.white {
-        didSet {
-            setNeedsLayout()
-        }
-    }
-    
     @IBInspectable var startPoint: CGPoint = CGPoint(x: 0.5, y: 0) {
         didSet {
             setNeedsLayout()
         }
     }
     
+    @IBInspectable var endColor: UIColor = UIColor.white {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
     @IBInspectable var endPoint: CGPoint = CGPoint(x: 0.5, y: 1) {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.black {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var radius: CGFloat = 0 {
         didSet {
             setNeedsLayout()
         }
@@ -50,15 +68,21 @@ class GradationView: UIView {
         if let gradient = self.gradient {
             let startColor = self.startColor
             let endColor = self.endColor
+            
             gradient.colors = [startColor.cgColor, endColor.cgColor]
             gradient.startPoint = CGPoint(x: startPoint.x, y: startPoint.y)
             gradient.endPoint = CGPoint(x: endPoint.x, y: endPoint.y)
+            
+            gradient.cornerRadius = radius
+            gradient.borderWidth = borderWidth
+            gradient.borderColor = borderColor.cgColor
         }
     }
     
     
     
-    // 뷰의 자동크기 조절을 위해 layoutSubviews의 메소드를 이용
+    // 모든 뷰의 크기에 맞게 조절하기 위해 layoutSubviews의 메소드를 이용
+    // 이를 사용하지 않으면 하나의 고정된 뷰의 크기대로 나온다.
     override func layoutSubviews() {
         initGradation()
         updateGradient()
